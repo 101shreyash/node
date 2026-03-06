663e62f53331b12424a1055fb1fc1d3df3d7f71e
Shreyash <himeshreyash@gmail.com>
2026-03-06 15:40:39 +0545

    http server

--- 02node/index.js
+++ 02node/index.js
@@ -1,4 +1,16 @@
+import { log } from "console"
 import http from "http"
 
 
-const myserver = http.createServer()
+const myserver = http.createServer((req,res)=>{
+
+console.log("Request Received");
+console.log(req); // req itself is an obj , where info requests info are stored
+
+console.log(req.headers);
+res.end("Hello from Nodeserver")
+ 
+})
+
+
+myserver.listen(8000 , ()=> {console.log("Server Started!");})
